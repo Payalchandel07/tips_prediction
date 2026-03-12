aa019eae9f6e83549f86fa505b227aca5b897511
Payalchandel07 <payalchandel964@gmail.com>
2026-03-12 16:07:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ model=pickle.load(open('tips_model.pkl','rb'))
 st.title("Tip Prediction app")
 total_bill=st.number_input("Total Bill")
 size=st.number_input("size")
-sex=st.selectbox('sex',['male','female'])
+sex=st.selectbox('sex',['Male','Demale'])
 smoker=st.selectbox('smoker',['yes','no'])
-day=st.selectbox('day',['thur','fri','sat','sun'])
-time=st.selectbox('time',['lunch','dinner'])
+day=st.selectbox('day',['Thur','Fri','Sat','Sun'])
+time=st.selectbox('time',['Lunch','Dinner'])
 input_data=pd.DataFrame({
     "total_bill":[total_bill],
     'size':[size],
